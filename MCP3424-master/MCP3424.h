f04482a75e73c86244792d4dd8c65467547a149b
hellmutbrudny <hellmut@cbros.com>
2023-03-06 20:34:52 +0100

    MCP3424 using Wire1

--- MCP3424-master/MCP3424.h
+++ MCP3424-master/MCP3424.h
@@ -1,10 +1,10 @@
 /*
  *  Copyright (C) 2014 Bernhard Schneider <bernhard@neaptide.org>
- *   
+ *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
  *  version 3 as published by the Free Software Foundation.
- * 
+ *
  */
 
 #ifndef __MCP3424_h__
@@ -15,11 +15,12 @@ extern "C" {
 #include <inttypes.h>
 }
 
+#include <Adafruit_I2CDevice.h>
 #include <Wire.h>
 
 typedef enum { ONE_SHOT, CONTINUOUS } ConvType;
 typedef enum { CH1, CH2, CH3, CH4 } Channel;
-typedef enum { R12B, R14B, R16B, R18B } Resolution; 
+typedef enum { R12B, R14B, R16B, R18B } Resolution;
 typedef enum { GAINx1, GAINx2, GAINx4, GAINx8 } Gain;
 typedef enum { PIN_LOW, PIN_HIGH, PIN_FLOAT } PinType;
 typedef enum { R_OK, R_UNDERFLOW, R_OVERFLOW, R_I2C, R_IN_PROGRESS, R_TIMEOUT } ConvStatus;
@@ -27,7 +28,7 @@ typedef enum { GC_LATCH=0x04, GC_RESET=0x06, GC_CONVERSION=0x08 } GCall_t;
 
 //const uint16_t  conv_time[]  = {1000/176, 1000/44, 1000/11, 100000/275};
 const uint16_t  conv_time[]  = { 30, 40, 90, 290}; // FIXME: have to be hw indep.
-const uint8_t pin_addr[] = { 
+const uint8_t pin_addr[] = {
     0b1101000, 0b1101010, 0b1101001,
     0b1101100, 0b1101110, 0b1101101,
     0b1101011, 0b1101111, 0b1101000 };
@@ -55,6 +56,7 @@ typedef struct  {
 class MCP3424 {
 
   private:
+            TwoWire *twoWire;
             uint8_t   addr;
             _ConfReg  cread, cwrite;
 
@@ -67,6 +69,8 @@ class MCP3424 {
             MCP3424 (uint8_t);
 
             MCP3424 (PinType, PinType);
+
+            void begin(TwoWire *wire = &Wire);
 
             uint8_t generalCall(GCall_t) const;
 
