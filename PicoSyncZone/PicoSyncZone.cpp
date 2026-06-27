298be058e3cf264b1ebc326229501c3f81c255bd
Andrzej Tomczyk <atomczyk@7bulls.com>
2026-06-27 09:05:57 +0200

    Software mutex in PicoSyncZone

--- PicoSyncZone/PicoSyncZone.cpp
+++ PicoSyncZone/PicoSyncZone.cpp
@@ -1,13 +1,37 @@
 #include <PicoSyncZone.h>
+#include <pico/platform.h>
+#include <Arduino.h>
 
-PicoSyncZone::PicoSyncZone() {
-  mutex_init(&_mutex);
-}
+PicoSyncZone::PicoSyncZone() {}
+
+// Software Locking (Peterson's / Dekker's Algorithm)
 
 void PicoSyncZone::enter() {
-  mutex_enter_blocking(&_mutex);
+  int thread_id = get_core_num();
+  int other = 1 - thread_id;
+  // Announce the intention to enter
+  flag[thread_id] = true;
+  turn = other;
+  int busy = 0;
+  // Busy-wait (spin) while the other thread is active and it is their turn
+  while (flag[other]) {
+    if (turn == other) {
+      if (!busy) {
+        busy = 1;
+        Serial.printf("!!! Other core %i keeps the lock\n", other);
+      }
+      flag[thread_id] = false;
+      while (turn == other);
+      flag[thread_id] = true;
+    }
+  }
+  if (busy) {
+    Serial.printf("!!! Other core %i released the lock\n", other);
+  }
 }
 
 void PicoSyncZone::exit() {
-  mutex_exit(&_mutex);
+  int thread_id = get_core_num();
+  int other = 1 - thread_id;
+  flag[thread_id] = false;
 }
--- PicoSyncZone/PicoSyncZone.h
+++ PicoSyncZone/PicoSyncZone.h
@@ -1,6 +1,6 @@
 #ifndef _PicoSyncZone_h_
 #define _PicoSyncZone_h_
-#include <pico/multicore.h>
+// #include <pico/multicore.h>
 
 class PicoSyncZone {
   public:
@@ -8,7 +8,8 @@ class PicoSyncZone {
     void enter();
     void exit();
   private:
-    mutex_t _mutex;
+    bool flag[2] = {false, false};
+    int turn = 0;
 };
 
 #endif
